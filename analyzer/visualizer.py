b43be0579c9e6465832665fcce9a5110252ac3ca
Kateryna Koziar <katya.kozyar.333@gmail.com>
2024-01-10 18:01:48 +0200

    Add class for cleaning data to visualizer.py

--- analyzer/visualizer.py
+++ analyzer/visualizer.py
@@ -7,3 +7,11 @@ class DataReader:
     def read_data(file_path):
         data = pd.read_csv(file_path)
         return data
+
+
+class DataCleaner:
+    @staticmethod
+    def clean_data(data):
+        data.dropna(subset=["Technologies"], inplace=True)
+        data["Technologies"] = data["Technologies"].apply(lambda x: x.split(', ') if isinstance(x, str) else [])
+        return data.explode("Technologies")
