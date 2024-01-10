b03bef546cae9796ed2ca1d6dac1a623d8e119b4
Kateryna Koziar <katya.kozyar.333@gmail.com>
2024-01-10 17:56:39 +0200

    Add functions for parsing nessesary data to scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -49,3 +49,64 @@ class JobScraper:
                 break
 
         driver.quit()
+
+    def parse_job(self, job):
+        title_element = job.find_element(By.CLASS_NAME, "job-list-item__link")
+        title = title_element.text.strip()
+        try:
+            more_details_button = job.find_element(By.CSS_SELECTOR, "[data-toggle='show-more']")
+            more_details_button.click()
+            WebDriverWait(job, 5).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "[data-original-text]"))
+            )
+        except NoSuchElementException:
+            pass
+        description_element = job.find_element(By.CLASS_NAME, "job-list-item__description")
+        description = description_element.text.lower()
+        print(description)
+        experience = self.parse_experiance(job)
+        found_techs = self.find_technologies(description)
+
+        return {
+            "title": title,
+            "technologies": found_techs,
+            "experience": experience
+        }
+
+    def find_technologies(self, description):
+        description = re.sub(r'[^a-zA-Z]', '', description).lower()
+        return [tech for tech in self.technologies if tech.lower() in description]
+
+    def parse_experiance(self, job):
+        try:
+            job_info_element = job.find_element(By.CLASS_NAME, "job-list-item__job-info")
+            job_info = job_info_element.text.lower()
+            job_title_element = job.find_element(By.CLASS_NAME, "job-list-item__link")
+            job_title = job_title_element.text.lower()
+        except NoSuchElementException:
+            print("Necessary elements not found in job listing.")
+            return "Not specified"
+
+        text_to_check = job_info + " " + job_title
+        levels = ["junior", "senior", "middle"]
+
+        for level in levels:
+            if level in text_to_check:
+                return level
+
+        years_of_experience_keywords = ['year', 'years', 'рік', 'роки', 'років']
+        for word in text_to_check.split():
+            if word.isdigit():
+                next_word_index = text_to_check.split().index(word) + 1
+                if (next_word_index < len(text_to_check.split()) and
+                        text_to_check.split()[next_word_index] in
+                        years_of_experience_keywords):
+                    years_of_experience = int(word)
+                    if years_of_experience <= 1:
+                        return "junior"
+                    elif 1 < years_of_experience <= 3:
+                        return "middle"
+                    else:
+                        return "senior"
+
+        return "Not specified"
